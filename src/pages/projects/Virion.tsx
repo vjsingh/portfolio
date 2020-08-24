d3ec361ec3420c861ce081821e51e645f70abda1
vjsingh <varun101@gmail.com>
2020-08-24 09:29:52 -0400

    Fix small typo

--- src/pages/projects/Virion.tsx
+++ src/pages/projects/Virion.tsx
@@ -13,7 +13,7 @@ const name = 'virion';
 const headerText = 'Virion';
 const subheaderText = 'Enchanting online immunology game.';
 const linkHref = 'http://www.itsvirion.com';
-const overviewText = `Virion is an immersive online game that features enchanting audio, deceptively simple gameplay, and broodily gorgeous visuals. As the antagonist, you play as a single virus cell, or a Virion, attempting to infect the human body. Your only hope is to continue infecting new host cells before the immune system can kill off your remaining virotic cells.\n\nVenga was the winner of the 2011 Zynga Hidden Agenda game competition. While playing the game players intuitively learn about the basics of immunology, including B cells, T cells, macrophages, antibodies, and mutations.\n\nVirion was made using HTML5 and processing.js before it was cool.`;
+const overviewText = `Virion is an immersive online game that features enchanting audio, deceptively simple gameplay, and broodily gorgeous visuals. As the antagonist, you play as a single virus cell, or a Virion, attempting to infect the human body. Your only hope is to continue infecting new host cells before the immune system can kill off your remaining virotic cells.\n\nVirion was the winner of the 2011 Zynga Hidden Agenda game competition. While playing the game players intuitively learn about the basics of immunology, including B cells, T cells, macrophages, antibodies, and mutations.\n\nVirion was made using HTML5 and processing.js before it was cool.`;
 const color = getPageColor(name);
 
 const Virion: React.FC<PageProps> = props => {
